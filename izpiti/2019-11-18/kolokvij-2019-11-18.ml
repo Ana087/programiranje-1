f7b3396cf7a8937345b71f25046bfe3dddea1ca9
Ana Medved <ana.medved@student.fmf.uni-lj.si>
2019-11-25 10:14:07 +0100

    kolokvij

--- izpiti/2019-11-18/kolokvij-2019-11-18.ml
+++ izpiti/2019-11-18/kolokvij-2019-11-18.ml
@@ -2,26 +2,59 @@
 (* 1. naloga: funkcije *)
 (* =================== *)
 
-let is_root () = failwith "dopolni me"
+let is_root x y = if x * x = y && x >= 0 then true else false
 
-let pack3 () = failwith "dopolni me"
+let pack3 a b c = (a, b, c)
 
-let sum_if_not () = failwith "dopolni me"
+let rec sum_if_not f list = 
+  let rec aux_sum f list acc =
+  match list with
+  | [] -> acc
+  | x :: xs -> if f x then aux_sum f xs acc else aux_sum f xs (acc + x)
+in aux_sum f list 0
 
-let apply () = failwith "dopolni me"
+
+let rec inverz list =
+  match list with
+  | [] -> []
+  | x :: xs -> inverz xs @ [x]
+
+let rec uporabi_funkcije list m =
+  let rec uporabi_aux list m acc =
+    match list with
+    | [] -> inverz acc
+    | x :: xs -> uporabi_aux xs m ([x m] @ acc)
+  in uporabi_aux list m [] 
+
+let rec apply list1 list2 =
+  let rec apply_aux list1 list2 acc = 
+    match (list1, list2) with
+    | (_, []) -> inverz acc
+    | (_, x :: xs) -> apply_aux list1 xs ([(uporabi_funkcije list1 x)] @ acc)
+  in apply_aux list1 list2 []
+
+(* Inverz ne dela, funkcija brez inverza deluje.)
 
 (* ======================================= *)
 (* 2. naloga: podatkovni tipi in rekurzija *)
 (* ======================================= *)
 
-type vrsta_srecanja = DopolniMe
+type vrsta_srecanja = 
+  | Predavanja
+  | Vaje
 
-type srecanje = DopolniMe'
+type srecanje = {predmet : string; vrsta : vrsta_srecanja; trajanje : int}
 
-let vaje () = failwith "dopolni me"
-let predavanje () = failwith "dopolni me"
+type urnik =
+  | Srecanje of list * list
 
-let urnik_profesor () = failwith "dopolni me"
+let vaje = {predmet = "Analiza2a"; vrsta = Vaje; trajanje = 3}
+
+let predavanje = {predmet = "Programiranje 1"; vrsta = Predavanja; trajanje = 2}
+
+
+
+let urnik_profesor = [{predmet = "Programiranje 1"; vrsta = Vaje; trajanje = 2}; {predmet = "Analiza 1"; vrsta = Predavanja; trajanje = 1}; {predmet = "Algebra"; vrsta = Vaje; trajanje = 1}]
 
 let je_preobremenjen () = failwith "dopolni me"
 
